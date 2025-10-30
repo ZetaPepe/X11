374c0e5590d4661e0094dda5419efd9306823e33
ChristAI <158454707+ZetaPepe@users.noreply.github.com>
2025-10-30 10:30:01 +0800

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -6,8 +6,8 @@ export const middleware = paymentMiddleware(
   "0x8b12a3c1AC1D1356cB24A642daFCA8f0f60cDeF2", // your receiving wallet address
   {  // Route configurations for protected endpoints
     '/protected': {
-      price: '$0.01',
-      network: "base-sepolia", // for mainnet, see Running on Mainnet section
+      price: '$0.05',
+      network: "base", // for mainnet, see Running on Mainnet section
       config: {
         description: 'Access to protected content',
         // Optional: Add schemas for better discovery
@@ -35,3 +35,4 @@ export const config = {
     '/protected/:path*',
   ]
 };
+
